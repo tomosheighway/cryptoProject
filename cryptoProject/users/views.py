704e5b27b62336bdff5717c471d7aa68317f5329
Tomos Heighway <tomosheighway@gmail.com>
2023-03-27 20:17:21 +0100

    output balance and value of eth and btc wallets

--- cryptoProject/users/views.py
+++ cryptoProject/users/views.py
@@ -7,22 +7,55 @@ import requests
 from django.shortcuts import render
 from django.contrib import messages
 from users.models import Portfolio 
+from decimal import Decimal
 
 def get_bitcoin_price():
     url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=gbp'
     response = requests.get(url).json()
     return response['bitcoin']['gbp']
 
-def get_bitcoin_balance():
-    url = 'https://blockchain.info/rawaddr/16ftSEQ4ctQFDtVZiUBusQUjRrGhM3JYwe'
+def get_crypto_prices():
+    url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=gbp'
+    response = response = requests.get(url).json()
+    return response
+
+# https://blockchain.info/rawaddr/{wallet_address}                alternataive endpoint 
+def get_bitcoin_balance(wallet_address):
+    url = f'https://api.blockcypher.com/v1/btc/main/addrs/{wallet_address}/balance'
     response = requests.get(url).json()
-    return response["final_balance"]
+    return response["final_balance"] / 100000000 
+
+    # value in satoshi  convert by div    e.g  = balance / 100000000
+# def get_bitcoin_balance():
+#     url = 'https://api.blockcypher.com/v1/btc/main/addrs/16ftSEQ4ctQFDtVZiUBusQUjRrGhM3JYwe/balance'
+#     # https://blockchain.info/rawaddr/16ftSEQ4ctQFDtVZiUBusQUjRrGhM3JYwe
+#     response = requests.get(url).json()
+#     print(response) 
+#     return response["final_balance"]
+
+def get_ethereum_balance(wallet_address):
+    payload = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "eth_getBalance",
+        "params": [wallet_address, "latest"]
+    }
+    response = requests.post("https://mainnet.infura.io/v3/abf01257db754eb0ac8234697a7a414d", json=payload)
+    response.raise_for_status()  # raise an exception if the HTTP request failed
+    balance_wei = int(response.json()['result'], 16)
+        # convert the balance to Ether
+    balance_eth = balance_wei / 10**18
+    print(balance_eth)  # print the balance to the console
+    return balance_eth
+    
 
 
 def home(request):
+    temp_wallet = "16ftSEQ4ctQFDtVZiUBusQUjRrGhM3JYwe"
     bitcoin_price = get_bitcoin_price()
-    bitcoin_balance = get_bitcoin_balance()
-    return render(request, 'users/home.html', {'bitcoin_price': bitcoin_price, 'bitcoin_balance': bitcoin_balance})
+    # bitcoin_balance = get_bitcoin_balance(temp_wallet)
+    return render(request, 'users/home.html', {'bitcoin_price': bitcoin_price})
+    #return render(request, 'users/home.html', {'bitcoin_price': bitcoin_price, 'bitcoin_balance': bitcoin_balance})
 
 
 def register(request):
@@ -54,6 +87,27 @@ def portfolio(request):
         return redirect('portfolio')
     else:
         portfolios = Portfolio.objects.filter(user=request.user)
+        # bitcoin_price = get_bitcoin_price()
+        crypto_prices = get_crypto_prices()
+        bitcoin_price = crypto_prices['bitcoin']['gbp']
+        ethereum_price = crypto_prices['ethereum']['gbp']
+
+
+        for portfolio in portfolios:
+            if portfolio.currency.lower() == "btc":
+                balance = get_bitcoin_balance(portfolio.address)
+                portfolio.balance = balance 
+                portfolio.value = balance * bitcoin_price
+            
+            elif portfolio.currency.lower() == "eth":
+                balance = get_ethereum_balance(portfolio.address)
+                if balance is not None:
+                    portfolio.balance = balance
+                    portfolio.value = balance * ethereum_price
+                else:
+                    messages.warning(request, "Failed to get Ethereum balance.")
+            else:
+                messages.warning(request, f"{portfolio.currency} is not supported.")
         
     return render(request, 'users/portfolio.html', {'portfolios': portfolios})
 
