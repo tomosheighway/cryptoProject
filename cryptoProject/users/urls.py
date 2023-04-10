85526ca2268dfe4b31d3f9d607a64770a858ba04
Tomos Heighway <tomosheighway@gmail.com>
2023-04-10 21:27:26 +0100

    small style changes / navigation for logout

--- cryptoProject/users/urls.py
+++ cryptoProject/users/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('bitcoin-chart/', views.bitcoin_chart, name='bitcoin_chart'),
     path('ethereum-chart/', views.ethereum_chart, name='ethereum_chart'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
-    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('logout/', auth_view.LogoutView.as_view(next_page='home'), name="logout"),
     path('delete_account/', views.delete_account, name='delete_account'),
     path('delete-user/', views.delete_user, name='delete_user'),
   
